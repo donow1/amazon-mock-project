ce15dd0a80aa869dcf660277a969b40b24112499
Ella <ellaLyang@yahoo.com>
2024-11-30 11:05:27 +0800

    render the check-out section

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -15,3 +15,20 @@ export const deliveryOptions = [{
 }
 ];
 
+export function getDeliveryOption(deliveryOptionId){
+    let deliveryOption;
+
+    deliveryOptions.forEach((option)=>{
+      if(option.id === deliveryOptionId){
+        deliveryOption = option;
+      }
+    });
+    return deliveryOption || deliveryOptions[0];
+}
+
+
+
+
+
+
+
